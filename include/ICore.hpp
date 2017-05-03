c828219c880fb2f5b652b632b27063f33f36d67b
Yassir Jabbari <yassir.jabbari@epitech.eu>
2017-05-03 14:29:40 +0200

    Modification ICore

--- include/ICore.hpp
+++ include/ICore.hpp
@@ -7,16 +7,23 @@
 ** Started on  Wed May 03 13:24:18 2017 Yassir Jabbari
 ** Last update Wed May 03 13:24:18 2017 Yassir Jabbari
 */
+
 #ifndef CPP_INDIE_STUDIO_ICORE_HPP
 #define CPP_INDIE_STUDIO_ICORE_HPP
 # include <iostream>
 # include <vector>
+# include <irrlicht.h>
+# include <IImageLoader.h>
+
 class ICore
 {
  public:
   virtual ~ICore() {};
-  virtual std::vector<std::string> getSaves() const;
-  virtual void setSave(const std::string &);
+  virtual std::vector<std::string> &getSaves() const = 0;
+  virtual void setSave(const std::string &) = 0;
+  virtual void fillSaves() = 0;
+  virtual void fillTextures() = 0;
+  virtual std::vector<irr::video::IImage *> &getTextures() const = 0;
 };
 
 #endif //CPP_INDIE_STUDIO_ICORE_HPP

ec742fd98870bd22164018ad133193a1728e655d
Loïc Dupil <loic.dupil@epitech.eu>
2017-05-08 13:21:38 +0200

    events added

--- include/EventStatus.hpp
+++ include/EventStatus.hpp
@@ -5,7 +5,7 @@
 // Login   <loic.lopez@epitech.eu>
 //
 // Started on  lun. mai 08 12:51:59 2017 Loïc Lopez
-// Last update lun. mai 08 12:51:59 2017 Loïc Lopez
+// Last update Mon May  8 13:21:10 2017 Loïc Dupil
 //
 
 #ifndef CPP_INDIE_STUDIO_EVENTSTATUS_HPP
@@ -18,7 +18,12 @@ enum class EventStatus : uint16_t
   STAND_BY = 0,
   ENTER_IN_GAME = 1,
   BACK_TO_MENU = 2,
-  QUIT = 3
+  IN_GAME_MENU = 3,
+  SETTINGS = 4,
+  SAVES = 5,
+  LOAD = 6,
+  QUIT = 7,
+  SPLASH = 8
 };
 
 #endif //CPP_INDIE_STUDIO_EVENTSTATUS_HPP

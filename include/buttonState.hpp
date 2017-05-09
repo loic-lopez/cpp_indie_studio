ff3466899a19f45e00da75c30751e133e598ec46
Loïc Lopez <loic.lopez@epitech.eu>
2017-05-09 18:52:02 +0200

    add enum

---
+++ include/buttonState.hpp
@@ -0,0 +1,20 @@
+//
+// buttonState.hpp for cpp_indie_studio in /home/lopez_i/cpp_indie_studio/buttonState.hpp
+//
+// Made by Loïc Lopez
+// Login   <loic.lopez@epitech.eu>
+//
+// Started on  mar. mai 09 18:38:41 2017 Loïc Lopez
+// Last update mar. mai 09 18:38:41 2017 Loïc Lopez
+//
+
+#ifndef CPP_INDIE_STUDIO_BUTTONSTATE_HPP
+#define CPP_INDIE_STUDIO_BUTTONSTATE_HPP
+
+enum buttonState
+{
+  BS_UP,
+  BS_DOWN
+};
+
+#endif //CPP_INDIE_STUDIO_BUTTONSTATE_HPP
